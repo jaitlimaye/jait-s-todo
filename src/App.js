f66628a1f59205d32bac5e32da1b709641422a00
Jait <jaitlimaye@gmail.com>
2023-04-06 11:55:55 -0700

    Added basic navigation and styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Daily from './components/daily';
+import Longterm from './components/longterm';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+    <div className="vh-100 d-flex flex-column">
+      <div class="container h-100 pt-3">
+        <ul class="nav nav-tabs nav-fill"id="myTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <Link to="/daily" style={{ textDecoration: 'none'}}>
+              <button class="nav-link active" id="daily-tab" data-bs-toggle="tab" data-bs-target="#daily" type="button" role="tab" aria-controls="daily" aria-selected="true" style={{color:"#495057"}}>Daily</button>
+            </Link>
+          </li>
+          <li class="nav-item" role="presentation">
+            <Link to="/longterm" style={{ textDecoration: 'none' }}>
+              <button class="nav-link" id="longterm-tab" data-bs-toggle="tab" data-bs-target="#longterm" type="button" role="tab" aria-controls="longterm" aria-selected="false" style={{color:"#495057"}}>Long term</button>
+            </Link>
+          </li>
+        </ul>
+        <div class="container border-start border-end h-100" >
+          <Routes>
+            <Route path="/" element={<Daily/>} />
+            <Route path="/daily"element={<Daily/>}/>
+            <Route path="/longterm" element={<Longterm/>} />
+          </Routes>
+        </div>
+      </div>
+     </div>
+  </BrowserRouter>
   );
 }
 
---
+++ src/components/longterm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Longterm = () => {
+  return (
+    <div>Longterm</div>
+  )
+}
+
+export default Longterm
